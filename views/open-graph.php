b5fcc9dddc290faaba10ba6f9cab40087f53e94b
Nikolas <nikoski89@hotmail.com>
2013-10-28 16:56:41 -0300

    Agregando Open Graph

--- views/open-graph.php
+++ views/open-graph.php
@@ -1,7 +1,7 @@
 <meta property="og:title" content="<?=$openGraph['titulo']?>">
 <meta property="og:type" content="website">
 <meta property="og:url" content="<?='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>">
-<meta property="og:image" content="http://www.jzmusic.cl/stage/imgs/logo-jzmusic-disco-160.png">
+<meta property="og:image" content="http://www.jzmusic.cl/stage/imgs/logo-jzmusic-disco-300.png">
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="160">
 <meta property="og:image:height" content="160">
