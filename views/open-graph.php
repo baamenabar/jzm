7497f37340e9a6831fd59fee0d401969395e11a3
Nikolas <nikoski89@hotmail.com>
2013-10-29 15:23:53 -0300

    Ruta imagen OG

--- views/open-graph.php
+++ views/open-graph.php
@@ -1,11 +1,7 @@
 <meta property="og:title" content="<?=$openGraph['titulo']?>">
 <meta property="og:type" content="website">
 <meta property="og:url" content="<?='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>">
-<<<<<<< HEAD
 <meta property="og:image" content="http://www.jzmusic.cl/imgs/logo-jzmusic-disco-300.png">
-=======
-<meta property="og:image" content="http://www.jzmusic.cl/stage/imgs/logo-jzmusic-disco-300.png">
->>>>>>> 755949cd798b85fc7d993c8a8127dac605b063c6
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="300">
 <meta property="og:image:height" content="300">
