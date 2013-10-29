16dd1ab0c5e50e9872f5c0d7213295d8f0541cea
Nikolas <nikoski89@hotmail.com>
2013-10-29 15:12:18 -0300

    Cambiando ruta imagen OG

---
+++ views/open-graph.php
@@ -0,0 +1,8 @@
+<meta property="og:title" content="<?=$openGraph['titulo']?>">
+<meta property="og:type" content="website">
+<meta property="og:url" content="<?='http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>">
+<meta property="og:image" content="http://www.jzmusic.cl/imgs/logo-jzmusic-disco-300.png">
+<meta property="og:image:type" content="image/png">
+<meta property="og:image:width" content="300">
+<meta property="og:image:height" content="300">
+<meta property="og:description" content="<?=$openGraph['descripcion']?>">
